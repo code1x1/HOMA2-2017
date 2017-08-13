69c0c9b01bb65f2aa02ae96f2181de484d007e81
Denis Behrends <denis-bd@outlook.de>
2017-08-13 17:19:41 +0200

    Praktikum 4 Aufgabe 1 gelöst

---
+++ P04.01/CKomplex.cpp
@@ -0,0 +1,52 @@
+#include "CKomplex.h"
+#include<math.h>
+
+CKomplex::CKomplex(double a, double b)
+{
+	this->a = a;
+	this->b = b;
+}
+
+CKomplex::CKomplex(double phi)
+{
+	this->a = 1 * cos(phi);
+	this->b = 1 * sin(phi);
+
+}
+
+double CKomplex::re()
+{
+	return a;
+}
+
+double CKomplex::im()
+{
+	return b;
+}
+
+double CKomplex::abs()
+{
+	return sqrt((a * a) + (b * b));
+}
+
+CKomplex::~CKomplex()
+{
+}
+
+CKomplex operator+(CKomplex c1, CKomplex c2)
+{
+	CKomplex erg(c1.re() + c2.re(), c1.im() + c2.im());
+	return erg;
+}
+
+CKomplex operator*(CKomplex c1, CKomplex c2)
+{
+	CKomplex erg(c1.re() * c2.re(), c1.im() * c2.im());
+	return erg;
+}
+
+CKomplex operator*(double d, CKomplex c2)
+{
+	CKomplex erg(d * c2.re(), d * c2.im());
+	return erg;
+}
---
+++ P04.01/CKomplex.h
@@ -0,0 +1,16 @@
+#pragma once
+class CKomplex
+{
+	double a, b;
+public:
+	CKomplex(double a, double b);
+	CKomplex(double phi);
+	double re();
+	double im();
+	double abs();
+	~CKomplex();
+};
+
+CKomplex operator+(CKomplex c1, CKomplex c2);
+CKomplex operator*(CKomplex c1, CKomplex c2);
+CKomplex operator*(double d, CKomplex c2);
